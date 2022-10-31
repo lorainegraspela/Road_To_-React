ff79ca6dd0757b2222717395fe4fd94e31a788da
lorainegraspela <116239467+lorainegraspela@users.noreply.github.com>
2022-10-31 18:26:11 +0800

    Add files via upload

---
+++ Graspela, Iris M/11. Handler Function in JSX/App.js
@@ -0,0 +1,12 @@
+const Search = () => {
+   const handleChange = (event) => {
+      console.log(event);
+};
+
+return (
+   <div>
+	<label htmlFor="search">Search: </label>
+	<input id="search" type="text" onChange={handleChange} />
+   </div>
+ );
+};
